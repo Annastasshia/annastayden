60202ea86d74e1e56e1c868bd83bbbb09e8f389e
Annastasshia <aames26@gmail.com>
2021-03-09 11:25:44 -0500

    debug

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ const PORT = process.env.PORT || 3000;
 
 // static files
 app.use(express.static(path.join(__dirname, 'public')));
-// app.use('/css', express.static(__dirname + 'public/css'))
-// app.use('/js', express.static(__dirname + 'public/js'))
-// app.use('/img', express.static(__dirname + 'public/img'))
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/js', express.static(__dirname + 'public/js'))
+app.use('/img', express.static(__dirname + 'public/img'))
 
 // routes       
 app.get('/', (req,res) => {
